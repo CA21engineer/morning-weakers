5d4acf9c0191180689c926e640505f5994cf1499
Ryuichi Fukami <fukaryu421@gmail.com>
2020-06-08 12:48:04 +0900

    add routing

--- lib/pages/all/all_page.dart
+++ lib/pages/all/all_page.dart
@@ -1,11 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:morning_weakers/pages/input_participant_info/input_participant_Info_page.dart';
+import 'package:morning_weakers/pages/new_profile/new_profile_page.dart';
+import 'package:morning_weakers/pages/profile_detail/profile_detail_page.dart';
 
 class AllPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    List<Widget> _pageList = [
+      InputParticipantInfoPage(),
+      NewProfilePage(),
+      ProfileDetailPage(),
+    ];
+
+    List<String> _pageRouteNames = [
+      '/inputParticipantInfo',
+      '/newProfile',
+      '/profileDetail',
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Text('All page'),
+      ),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+              margin: const EdgeInsets.all(5),
+              child: Card(
+                child: ListTile(
+                  title: Text(_pageList[index].toString()),
+                  onTap: () {
+                    Navigator.pushNamed(context, _pageRouteNames[index]);
+                  },
+                ),
+              ));
+        },
+        itemCount: _pageList.length,
       ),
     );
   }
--- lib/pages/input_participant_info/input_participant_Info_page.dart
+++ lib/pages/input_participant_info/input_participant_Info_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class InputParticipantInfoPage extends StatelessWidget {
+  static const String pageName = 'Input participant info page';
+
   @override
   Widget build(BuildContext context) {
     return Container();
