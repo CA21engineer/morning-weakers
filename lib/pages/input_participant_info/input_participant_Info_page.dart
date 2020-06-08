a507125074a665b130b3d2456b4f766955dd3362
Ryuichi Fukami <fukaryu421@gmail.com>
2020-06-08 12:49:11 +0900

    modify each page start with Scaffold

--- lib/pages/input_participant_info/input_participant_Info_page.dart
+++ lib/pages/input_participant_info/input_participant_Info_page.dart
@@ -5,6 +5,6 @@ class InputParticipantInfoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
--- lib/pages/profile_detail/profile_detail_page.dart
+++ lib/pages/profile_detail/profile_detail_page.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class ProfileDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
