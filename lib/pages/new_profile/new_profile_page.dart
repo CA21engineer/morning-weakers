5a7906432775b95a5ee1cf611f38f452048b09d9
zoothezoo <kohekoshien@gmail.com>
2020-06-08 16:00:11 +0900

    implement base input profile page

--- lib/pages/new_profile/new_profile_page.dart
+++ lib/pages/new_profile/new_profile_page.dart
@@ -3,6 +3,68 @@ import 'package:flutter/material.dart';
 class NewProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('プロフィール新規作成'),
+      ),
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(30),
+          child: Column(
+            children: <Widget>[
+              _profileIcon(),
+              _profileDetail(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _profileIcon() {
+    return Center(
+      child: Container(
+        padding: const EdgeInsets.all(50.0),
+        child: Column(
+          children: <Widget>[
+            Icon(
+              Icons.people,
+              size: 100,
+            ),
+            RaisedButton(
+              onPressed: null,
+              child: Text('選択'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _profileDetail() {
+    return Column(
+      children: <Widget>[
+        TextFormField(
+          decoration: InputDecoration(
+            labelText: '名前',
+          ),
+        ),
+        TextFormField(
+          decoration: InputDecoration(
+            labelText: 'ハンドルネーム',
+          ),
+        ),
+        TextFormField(
+          decoration: InputDecoration(
+            labelText: 'twitter アカウント',
+          ),
+        ),
+        TextFormField(
+          decoration: InputDecoration(
+            labelText: 'github アカウント',
+          ),
+        ),
+      ],
+    );
   }
 }
