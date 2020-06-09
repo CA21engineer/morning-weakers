586dc78fd87ad8078c33ef0004b8b6342504fa0d
ho2ri2s <ho2ri2s.github@gmail.com>
2020-06-09 20:42:37 +0900

    add: hackathon model

---
+++ lib/model/hackathon/hackathon.freezed.dart
@@ -0,0 +1,355 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named
+
+part of 'hackathon.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+Hackathon _$HackathonFromJson(Map<String, dynamic> json) {
+  return _Hackathon.fromJson(json);
+}
+
+class _$HackathonTearOff {
+  const _$HackathonTearOff();
+
+  _Hackathon call(
+      {@required String id,
+      @required String title,
+      @required String description,
+      @required String theme,
+      @required @JsonKey(name: 'icon_url') String iconUrl,
+      @required @JsonKey(name: 'slack_url') String slackUrl,
+      @required @JsonKey(name: 'presentation_url') String presentationUrl,
+      @required @JsonKey(name: 'state_date') DateTime startDate,
+      @required @JsonKey(name: 'end_date') DateTime endDate,
+      @required int span}) {
+    return _Hackathon(
+      id: id,
+      title: title,
+      description: description,
+      theme: theme,
+      iconUrl: iconUrl,
+      slackUrl: slackUrl,
+      presentationUrl: presentationUrl,
+      startDate: startDate,
+      endDate: endDate,
+      span: span,
+    );
+  }
+}
+
+// ignore: unused_element
+const $Hackathon = _$HackathonTearOff();
+
+mixin _$Hackathon {
+  String get id;
+  String get title;
+  String get description;
+  String get theme;
+  @JsonKey(name: 'icon_url')
+  String get iconUrl;
+  @JsonKey(name: 'slack_url')
+  String get slackUrl;
+  @JsonKey(name: 'presentation_url')
+  String get presentationUrl;
+  @JsonKey(name: 'state_date')
+  DateTime get startDate;
+  @JsonKey(name: 'end_date')
+  DateTime get endDate;
+  int get span;
+
+  Map<String, dynamic> toJson();
+  $HackathonCopyWith<Hackathon> get copyWith;
+}
+
+abstract class $HackathonCopyWith<$Res> {
+  factory $HackathonCopyWith(Hackathon value, $Res Function(Hackathon) then) =
+      _$HackathonCopyWithImpl<$Res>;
+  $Res call(
+      {String id,
+      String title,
+      String description,
+      String theme,
+      @JsonKey(name: 'icon_url') String iconUrl,
+      @JsonKey(name: 'slack_url') String slackUrl,
+      @JsonKey(name: 'presentation_url') String presentationUrl,
+      @JsonKey(name: 'state_date') DateTime startDate,
+      @JsonKey(name: 'end_date') DateTime endDate,
+      int span});
+}
+
+class _$HackathonCopyWithImpl<$Res> implements $HackathonCopyWith<$Res> {
+  _$HackathonCopyWithImpl(this._value, this._then);
+
+  final Hackathon _value;
+  // ignore: unused_field
+  final $Res Function(Hackathon) _then;
+
+  @override
+  $Res call({
+    Object id = freezed,
+    Object title = freezed,
+    Object description = freezed,
+    Object theme = freezed,
+    Object iconUrl = freezed,
+    Object slackUrl = freezed,
+    Object presentationUrl = freezed,
+    Object startDate = freezed,
+    Object endDate = freezed,
+    Object span = freezed,
+  }) {
+    return _then(_value.copyWith(
+      id: id == freezed ? _value.id : id as String,
+      title: title == freezed ? _value.title : title as String,
+      description:
+          description == freezed ? _value.description : description as String,
+      theme: theme == freezed ? _value.theme : theme as String,
+      iconUrl: iconUrl == freezed ? _value.iconUrl : iconUrl as String,
+      slackUrl: slackUrl == freezed ? _value.slackUrl : slackUrl as String,
+      presentationUrl: presentationUrl == freezed
+          ? _value.presentationUrl
+          : presentationUrl as String,
+      startDate:
+          startDate == freezed ? _value.startDate : startDate as DateTime,
+      endDate: endDate == freezed ? _value.endDate : endDate as DateTime,
+      span: span == freezed ? _value.span : span as int,
+    ));
+  }
+}
+
+abstract class _$HackathonCopyWith<$Res> implements $HackathonCopyWith<$Res> {
+  factory _$HackathonCopyWith(
+          _Hackathon value, $Res Function(_Hackathon) then) =
+      __$HackathonCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {String id,
+      String title,
+      String description,
+      String theme,
+      @JsonKey(name: 'icon_url') String iconUrl,
+      @JsonKey(name: 'slack_url') String slackUrl,
+      @JsonKey(name: 'presentation_url') String presentationUrl,
+      @JsonKey(name: 'state_date') DateTime startDate,
+      @JsonKey(name: 'end_date') DateTime endDate,
+      int span});
+}
+
+class __$HackathonCopyWithImpl<$Res> extends _$HackathonCopyWithImpl<$Res>
+    implements _$HackathonCopyWith<$Res> {
+  __$HackathonCopyWithImpl(_Hackathon _value, $Res Function(_Hackathon) _then)
+      : super(_value, (v) => _then(v as _Hackathon));
+
+  @override
+  _Hackathon get _value => super._value as _Hackathon;
+
+  @override
+  $Res call({
+    Object id = freezed,
+    Object title = freezed,
+    Object description = freezed,
+    Object theme = freezed,
+    Object iconUrl = freezed,
+    Object slackUrl = freezed,
+    Object presentationUrl = freezed,
+    Object startDate = freezed,
+    Object endDate = freezed,
+    Object span = freezed,
+  }) {
+    return _then(_Hackathon(
+      id: id == freezed ? _value.id : id as String,
+      title: title == freezed ? _value.title : title as String,
+      description:
+          description == freezed ? _value.description : description as String,
+      theme: theme == freezed ? _value.theme : theme as String,
+      iconUrl: iconUrl == freezed ? _value.iconUrl : iconUrl as String,
+      slackUrl: slackUrl == freezed ? _value.slackUrl : slackUrl as String,
+      presentationUrl: presentationUrl == freezed
+          ? _value.presentationUrl
+          : presentationUrl as String,
+      startDate:
+          startDate == freezed ? _value.startDate : startDate as DateTime,
+      endDate: endDate == freezed ? _value.endDate : endDate as DateTime,
+      span: span == freezed ? _value.span : span as int,
+    ));
+  }
+}
+
+@JsonSerializable()
+class _$_Hackathon with DiagnosticableTreeMixin implements _Hackathon {
+  const _$_Hackathon(
+      {@required this.id,
+      @required this.title,
+      @required this.description,
+      @required this.theme,
+      @required @JsonKey(name: 'icon_url') this.iconUrl,
+      @required @JsonKey(name: 'slack_url') this.slackUrl,
+      @required @JsonKey(name: 'presentation_url') this.presentationUrl,
+      @required @JsonKey(name: 'state_date') this.startDate,
+      @required @JsonKey(name: 'end_date') this.endDate,
+      @required this.span})
+      : assert(id != null),
+        assert(title != null),
+        assert(description != null),
+        assert(theme != null),
+        assert(iconUrl != null),
+        assert(slackUrl != null),
+        assert(presentationUrl != null),
+        assert(startDate != null),
+        assert(endDate != null),
+        assert(span != null);
+
+  factory _$_Hackathon.fromJson(Map<String, dynamic> json) =>
+      _$_$_HackathonFromJson(json);
+
+  @override
+  final String id;
+  @override
+  final String title;
+  @override
+  final String description;
+  @override
+  final String theme;
+  @override
+  @JsonKey(name: 'icon_url')
+  final String iconUrl;
+  @override
+  @JsonKey(name: 'slack_url')
+  final String slackUrl;
+  @override
+  @JsonKey(name: 'presentation_url')
+  final String presentationUrl;
+  @override
+  @JsonKey(name: 'state_date')
+  final DateTime startDate;
+  @override
+  @JsonKey(name: 'end_date')
+  final DateTime endDate;
+  @override
+  final int span;
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'Hackathon(id: $id, title: $title, description: $description, theme: $theme, iconUrl: $iconUrl, slackUrl: $slackUrl, presentationUrl: $presentationUrl, startDate: $startDate, endDate: $endDate, span: $span)';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('type', 'Hackathon'))
+      ..add(DiagnosticsProperty('id', id))
+      ..add(DiagnosticsProperty('title', title))
+      ..add(DiagnosticsProperty('description', description))
+      ..add(DiagnosticsProperty('theme', theme))
+      ..add(DiagnosticsProperty('iconUrl', iconUrl))
+      ..add(DiagnosticsProperty('slackUrl', slackUrl))
+      ..add(DiagnosticsProperty('presentationUrl', presentationUrl))
+      ..add(DiagnosticsProperty('startDate', startDate))
+      ..add(DiagnosticsProperty('endDate', endDate))
+      ..add(DiagnosticsProperty('span', span));
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _Hackathon &&
+            (identical(other.id, id) ||
+                const DeepCollectionEquality().equals(other.id, id)) &&
+            (identical(other.title, title) ||
+                const DeepCollectionEquality().equals(other.title, title)) &&
+            (identical(other.description, description) ||
+                const DeepCollectionEquality()
+                    .equals(other.description, description)) &&
+            (identical(other.theme, theme) ||
+                const DeepCollectionEquality().equals(other.theme, theme)) &&
+            (identical(other.iconUrl, iconUrl) ||
+                const DeepCollectionEquality()
+                    .equals(other.iconUrl, iconUrl)) &&
+            (identical(other.slackUrl, slackUrl) ||
+                const DeepCollectionEquality()
+                    .equals(other.slackUrl, slackUrl)) &&
+            (identical(other.presentationUrl, presentationUrl) ||
+                const DeepCollectionEquality()
+                    .equals(other.presentationUrl, presentationUrl)) &&
+            (identical(other.startDate, startDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.startDate, startDate)) &&
+            (identical(other.endDate, endDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.endDate, endDate)) &&
+            (identical(other.span, span) ||
+                const DeepCollectionEquality().equals(other.span, span)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(id) ^
+      const DeepCollectionEquality().hash(title) ^
+      const DeepCollectionEquality().hash(description) ^
+      const DeepCollectionEquality().hash(theme) ^
+      const DeepCollectionEquality().hash(iconUrl) ^
+      const DeepCollectionEquality().hash(slackUrl) ^
+      const DeepCollectionEquality().hash(presentationUrl) ^
+      const DeepCollectionEquality().hash(startDate) ^
+      const DeepCollectionEquality().hash(endDate) ^
+      const DeepCollectionEquality().hash(span);
+
+  @override
+  _$HackathonCopyWith<_Hackathon> get copyWith =>
+      __$HackathonCopyWithImpl<_Hackathon>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$_$_HackathonToJson(this);
+  }
+}
+
+abstract class _Hackathon implements Hackathon {
+  const factory _Hackathon(
+      {@required String id,
+      @required String title,
+      @required String description,
+      @required String theme,
+      @required @JsonKey(name: 'icon_url') String iconUrl,
+      @required @JsonKey(name: 'slack_url') String slackUrl,
+      @required @JsonKey(name: 'presentation_url') String presentationUrl,
+      @required @JsonKey(name: 'state_date') DateTime startDate,
+      @required @JsonKey(name: 'end_date') DateTime endDate,
+      @required int span}) = _$_Hackathon;
+
+  factory _Hackathon.fromJson(Map<String, dynamic> json) =
+      _$_Hackathon.fromJson;
+
+  @override
+  String get id;
+  @override
+  String get title;
+  @override
+  String get description;
+  @override
+  String get theme;
+  @override
+  @JsonKey(name: 'icon_url')
+  String get iconUrl;
+  @override
+  @JsonKey(name: 'slack_url')
+  String get slackUrl;
+  @override
+  @JsonKey(name: 'presentation_url')
+  String get presentationUrl;
+  @override
+  @JsonKey(name: 'state_date')
+  DateTime get startDate;
+  @override
+  @JsonKey(name: 'end_date')
+  DateTime get endDate;
+  @override
+  int get span;
+  @override
+  _$HackathonCopyWith<_Hackathon> get copyWith;
+}
---
+++ lib/model/hackathon/hackathon.g.dart
@@ -0,0 +1,40 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'hackathon.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Hackathon _$_$_HackathonFromJson(Map<String, dynamic> json) {
+  return _$_Hackathon(
+    id: json['id'] as String,
+    title: json['title'] as String,
+    description: json['description'] as String,
+    theme: json['theme'] as String,
+    iconUrl: json['icon_url'] as String,
+    slackUrl: json['slack_url'] as String,
+    presentationUrl: json['presentation_url'] as String,
+    startDate: json['state_date'] == null
+        ? null
+        : DateTime.parse(json['state_date'] as String),
+    endDate: json['end_date'] == null
+        ? null
+        : DateTime.parse(json['end_date'] as String),
+    span: json['span'] as int,
+  );
+}
+
+Map<String, dynamic> _$_$_HackathonToJson(_$_Hackathon instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'description': instance.description,
+      'theme': instance.theme,
+      'icon_url': instance.iconUrl,
+      'slack_url': instance.slackUrl,
+      'presentation_url': instance.presentationUrl,
+      'state_date': instance.startDate?.toIso8601String(),
+      'end_date': instance.endDate?.toIso8601String(),
+      'span': instance.span,
+    };
