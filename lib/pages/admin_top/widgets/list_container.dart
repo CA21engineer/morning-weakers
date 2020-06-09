2c9939560da5415e0b15c8881914b8f4c651db75
Ryuichi Fukami <fukaryu421@gmail.com>
2020-06-09 14:21:07 +0900

    add Divider

--- lib/pages/admin_top/widgets/list_container.dart
+++ lib/pages/admin_top/widgets/list_container.dart
@@ -11,13 +11,18 @@ class ListContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(
-        titleName,
-        style: const TextStyle(fontSize: 20, color: Colors.black),
-      ),
-      trailing: const Icon(Icons.arrow_forward_ios),
-      onTap: () => onTap,
+    return Column(
+      children: <Widget>[
+        ListTile(
+          title: Text(
+            titleName,
+            style: const TextStyle(fontSize: 20, color: Colors.black),
+          ),
+          trailing: const Icon(Icons.arrow_forward_ios),
+          onTap: () => onTap,
+        ),
+        const Divider(),
+      ],
     );
   }
 }
