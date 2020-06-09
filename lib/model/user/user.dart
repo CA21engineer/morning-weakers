390c18bb5a07c9b2a311d04ecbaa452cfd684ec5
ho2ri2s <ho2ri2s.github@gmail.com>
2020-06-10 00:14:41 +0900

    add: user model

--- lib/model/user/user.dart
+++ lib/model/user/user.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+
 part 'user.freezed.dart';
+
 part 'user.g.dart';
 
 @freezed
 abstract class User with _$User {
   const factory User({
     @required String id,
-    @required String nickname
-}) = _User;
+    @required @JsonKey(name: 'full_name') String fullName,
+    @required @JsonKey(name: 'display_name') String displayName,
+//  @required @JsonKey(name: 'technical_stack') List<TechnicalStack> technicalStack
+    @required @JsonKey(name: 'github_account') String githubAccount,
+    @required @JsonKey(name: 'twitter_account') String twitterAccount,
+    @required @JsonKey(name: 'icon_url') String iconUrl,
+  }) = _User;
 
- factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
 }
--- lib/model/user/user.g.dart
+++ lib/model/user/user.g.dart
@@ -9,11 +9,19 @@ part of 'user.dart';
 _$_User _$_$_UserFromJson(Map<String, dynamic> json) {
   return _$_User(
     id: json['id'] as String,
-    nickname: json['nickname'] as String,
+    fullName: json['full_name'] as String,
+    displayName: json['display_name'] as String,
+    githubAccount: json['github_account'] as String,
+    twitterAccount: json['twitter_account'] as String,
+    iconUrl: json['icon_url'] as String,
   );
 }
 
 Map<String, dynamic> _$_$_UserToJson(_$_User instance) => <String, dynamic>{
       'id': instance.id,
-      'nickname': instance.nickname,
+      'full_name': instance.fullName,
+      'display_name': instance.displayName,
+      'github_account': instance.githubAccount,
+      'twitter_account': instance.twitterAccount,
+      'icon_url': instance.iconUrl,
     };
