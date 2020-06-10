a96fd4916d7f1ea4dc1bc5ffe0b8cdde6e880c97
ho2ri2s <ho2ri2s.github@gmail.com>
2020-06-10 21:08:51 +0900

    add: get method

--- lib/repository/hackathon_repository.dart
+++ lib/repository/hackathon_repository.dart
@@ -12,48 +12,65 @@ class HackathonRepository {
     return Future.value(result);
   }
 
-  // TODO: authができたらUserはauthから取得する
-  Future<void> joinHackathon(
-    Hackathon hackathon,
-    List<TechnicalStack> desiredOccupations,
-    int workingDays,
-    String note,
-  ) async {
-    // hackathon documentに参加者を追加
-    final DocumentReference hackathonRef = firestore.collection('hackathons/v1').document(hackathon.id);
+  Future<Hackathon> getHackathon(String hackathonId) async {
+    final DocumentReference ref = firestore.collection('hackathons/v1').document(hackathonId);
+    // TODO: Exceptionを別クラスにまとめる
+    if (ref.get() == null) {
+      throw Exception('指定されたハッカソンは存在しません');
+    }
+    return ref.get().then((value) => Hackathon.fromJson(value.data));
+  }
 
-    final Participant participant = Participant(
-      id: hackathonRef.collection('participants').document().documentID,
-      user: dummyUser(),
-      desiredOccupations: desiredOccupations,
-      workingDays: workingDays,
-      note: note,
-      isAdmin: false,
-    );
-
-    await hackathonRef.setData(
-      <String, dynamic>{'participants': hackathon.copyWith(participants: hackathon.participants..add(participant))},
-    );
-
-    // Joined documentに新しいhackathonを追加
-    final CollectionReference joinedRef = firestore.collection('joined/v1');
-    joinedRef.where('user_id', isEqualTo: participant.user.id).snapshots().listen((event) {
-      if (event.documents.isEmpty) {
-        joinedRef.add(<String, dynamic>{
-          'user_id': participant.user.id,
-          'hackathon_ids': [hackathon.id],
-          'hackathon_icon_urls': [hackathon.iconUrl],
-        });
-      } else {
-        event.documents.forEach((doc) {
-          final String id = doc.documentID;
-          final List<String> hackathonIds = doc['hackathon_id'] as List<String>;
-          firestore
-              .collection('joined/v1')
-              .document(id)
-              .setData(<String, dynamic>{'hackathon_id': hackathonIds..add(hackathon.id)});
-        });
-      }
-    });
+  // if user is not belong to Hackathon, return null
+  Future<Joined> getMyHackathons() async {
+    // TODO: userIdはAuthができたら変更
+    final DocumentReference joinedRef = firestore.collection('joined/v1').document('userId');
+    return joinedRef.get().then((value) => Joined.fromJson(value.data));
   }
 }
+
+// TODO: authができたらUserはauthから取得する
+Future<void> joinHackathon(Hackathon hackathon,
+    List<TechnicalStack> desiredOccupations,
+    int workingDays,
+    String note,) async {
+  // hackathon documentに参加者を追加
+  final DocumentReference hackathonRef = firestore.collection('hackathons/v1').document(hackathon.id);
+
+  final Participant participant = Participant(
+    id: hackathonRef
+        .collection('participants')
+        .document()
+        .documentID,
+    user: dummyUser(),
+    desiredOccupations: desiredOccupations,
+    workingDays: workingDays,
+    note: note,
+    isAdmin: false,
+  );
+
+  await hackathonRef.setData(
+    <String, dynamic>{'participants': hackathon.copyWith(participants: hackathon.participants..add(participant))},
+  );
+
+  // Joined documentに新しいhackathonを追加
+  final CollectionReference joinedRef = firestore.collection('joined/v1');
+  joinedRef.where('user_id', isEqualTo: participant.user.id).snapshots().listen((event) {
+    if (event.documents.isEmpty) {
+      joinedRef.add(<String, dynamic>{
+        'user_id': participant.user.id,
+        'hackathon_ids': [hackathon.id],
+        'hackathon_icon_urls': [hackathon.iconUrl],
+      });
+    } else {
+      event.documents.forEach((doc) {
+        final String id = doc.documentID;
+        final List<String> hackathonIds = doc['hackathon_id'] as List<String>;
+        firestore
+            .collection('joined/v1')
+            .document(id)
+            .setData(<String, dynamic>{'hackathon_id': hackathonIds..add(hackathon.id)});
+      });
+    }
+  });
+}}
