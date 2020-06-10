3f13e9bf4a8371635c059287c0512aaf9ab21a2a
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2020-06-10 22:30:52 +0900

    Clean: 希望職種フォーム

--- lib/pages/input_participant_info/widgets/desired_occupation.dart
+++ lib/pages/input_participant_info/widgets/desired_occupation.dart
@@ -1,73 +1,38 @@
 import 'package:flutter/material.dart';
 
 class DesiredOccupation extends StatelessWidget {
+  final List<String> desires = ['参加希望職種(第1希望)', '参加希望職種(第2希望)', '参加希望職種(第3希望)' ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: <Widget>[
-        const Text('参加希望職種(第1希望)'),
-        DropdownButton<String>(
-        value: 'iOS',
-          icon: const Icon(Icons.arrow_downward),
-          iconSize: 24,
-          elevation: 16,
-          style: TextStyle(color: Theme.of(context).primaryColor),
-          underline: Container(
-            height: 2,
-            color: Theme.of(context).accentColor,
-          ),
-          onChanged: (String newValue) {},
-          items: <String>['iOS', 'Android', 'Webフロント', 'サーバーサイド']
-            .map<DropdownMenuItem<String>>((String value) =>
-              DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-              )
-            ).toList(),
-        ),
-        const Text('参加希望職種(第2希望)'),
-        DropdownButton<String>(
-          value: 'iOS',
-          icon: const Icon(Icons.arrow_downward),
-          iconSize: 24,
-          elevation: 16,
-          style: TextStyle(color: Theme.of(context).primaryColor),
-          underline: Container(
-            height: 2,
-            color: Theme.of(context).accentColor,
-          ),
-          onChanged: (String newValue) {},
-          // TODO: 既に選んだ職種は選択不可にする
-          items: <String>['iOS', 'Android', 'Webフロント', 'サーバーサイド']
-            .map<DropdownMenuItem<String>>((String value) =>
-              DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              )
-            ).toList(),
-        ),
-        const Text('参加希望職種(第3希望)'),
-        DropdownButton<String>(
-          value: 'iOS',
-          icon: const Icon(Icons.arrow_downward),
-          iconSize: 24,
-          elevation: 16,
-          style: TextStyle(color: Theme.of(context).primaryColor),
-          underline: Container(
-            height: 2,
-            color: Theme.of(context).accentColor,
-          ),
-          onChanged: (String newValue) {},
-          // TODO: 既に選んだ職種は選択不可にする
-          items: <String>['iOS', 'Android', 'Webフロント', 'サーバーサイド']
-            .map<DropdownMenuItem<String>>((String value) =>
-              DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              )
-            ).toList(),
-        ),
-      ],
+      children: desires.map((desire) =>
+        Column(
+          children: <Widget>[
+            Text(desire),
+            DropdownButton<String>(
+              value: 'iOS',
+              icon: const Icon(Icons.arrow_downward),
+              iconSize: 24,
+              elevation: 16,
+              style: TextStyle(color: Theme.of(context).primaryColor),
+              underline: Container(
+                height: 2,
+                color: Theme.of(context).accentColor,
+              ),
+              onChanged: (String newValue) {},
+              // TODO: 既に選んだ職種は選択不可にする
+              items: <String>['iOS', 'Android', 'Webフロント', 'サーバーサイド']
+                  .map<DropdownMenuItem<String>>((String value) =>
+                  DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  )
+              ).toList(),
+            ),
+          ],
+        )
+      ).toList(),
     );
   }
 }
