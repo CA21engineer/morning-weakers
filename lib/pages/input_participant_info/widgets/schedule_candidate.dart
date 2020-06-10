765b75ba3f6ba3c7649ebd441309faa06eda3b69
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2020-06-11 00:27:10 +0900

    Clean: 参加可能日程

--- lib/pages/input_participant_info/widgets/schedule_candidate.dart
+++ lib/pages/input_participant_info/widgets/schedule_candidate.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
 class ScheduleCandidate extends StatelessWidget {
+  final List<String> scheduleCandidates = [
+    '5/23(土)開始〜5/30(土)発表',
+    '5/24(日)開始〜5/31(日)発表',
+    '5/30(土)開始〜6/6(土)発表',
+    '5/31(日)開始〜6/7(日)発表'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -8,32 +15,18 @@ class ScheduleCandidate extends StatelessWidget {
       child: Column(
         children: <Widget>[
           const Text('参加可能日程(複数選択可)'),
-          CheckboxListTile(
-            value: false,
-//            onChanged: _handleCheckbox(),
-            activeColor: Theme.of(context).primaryColor,
-            title: const Text('5/23(土)開始〜5/30(土)発表'),
-          ),
-          CheckboxListTile(
-            value: false,
-//            onChanged: _handleCheckbox(),
-            activeColor: Theme.of(context).primaryColor,
-            title: const Text('5/24(日)開始〜5/31(日)発表'),
-          ),
-          CheckboxListTile(
-            value: false,
-//            onChanged: _handleCheckbox(),
-            activeColor: Theme.of(context).primaryColor,
-            title: const Text('5/30(土)開始〜6/6(土)発表'),
-          ),
-          CheckboxListTile(
-            value: false,
-//            onChanged: _handleCheckbox(),
-            activeColor: Theme.of(context).primaryColor,
-            title: const Text('5/31(日)開始〜6/7(日)発表'),
-          ),
-        ],
-      )
+          Column(
+            children: scheduleCandidates.map(
+              (scheduleCandidate) => CheckboxListTile(
+              value: false,
+  //            onChanged: _handleCheckbox(),
+              activeColor: Theme.of(context).primaryColor,
+              title: Text(scheduleCandidate),
+              ),
+            ).toList()
+          )
+        ]
+      ),
     );
   }
 }
