443f99661d830a177c33cee470057e933ffc3b05
Ryuichi Fukami <fukaryu421@gmail.com>
2020-06-11 01:10:50 +0900

    add TechnicalStackContainer content

--- lib/pages/profile_detail/widgets/technical_stack_container.dart
+++ lib/pages/profile_detail/widgets/technical_stack_container.dart
@@ -4,9 +4,11 @@ class TechnicalStackContainer extends StatelessWidget {
   const TechnicalStackContainer({
     @required this.titleName,
     @required this.contentName,
+    @required this.technicalStackTitles,
   });
 
   final String titleName, contentName;
+  final List<String> technicalStackTitles;
 
   @override
   Widget build(BuildContext context) {
@@ -22,18 +24,15 @@ class TechnicalStackContainer extends StatelessWidget {
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
           Column(
-            children: <Widget>[
-              Text(
-                contentName,
-                textAlign: TextAlign.right,
-                style: const TextStyle(fontSize: 20),
-              ),
-              Text(
-                contentName,
-                textAlign: TextAlign.right,
-                style: const TextStyle(fontSize: 20),
-              ),
-            ],
+            children: technicalStackTitles
+                .map(
+                  (technicalStackTitles) => Text(
+                    technicalStackTitles,
+                    textAlign: TextAlign.right,
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                )
+                .toList(),
           ),
         ],
       ),
