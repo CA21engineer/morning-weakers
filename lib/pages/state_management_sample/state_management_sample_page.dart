e944fa6a6242cf86c0fda8705e3cd4a72543905f
Ryuichi Fukami <fukaryu421@gmail.com>
2020-06-11 22:38:34 +0900

    add: StateManagementSamplePage

---
+++ lib/pages/state_management_sample/state_management_sample_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class StateManagementSamplePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('State管理'),
+      ),
+      body: Container(),
+    );
+  }
+}
