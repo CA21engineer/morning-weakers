c1e79a161da94327a3ad84082175f81abcfa679d
Ryuichi Fukami <fukaryu421@gmail.com>
2020-06-11 12:42:25 +0900

    add: content of dummyData

--- lib/pages/profile_detail/profile_detail_controller.dart
+++ lib/pages/profile_detail/profile_detail_controller.dart
@@ -1,6 +1,7 @@
 import 'package:morning_weakers/pages/profile_detail/profile_detail_state.dart';
 import 'package:state_notifier/state_notifier.dart';
 import 'package:morning_weakers/core/dummy_data.dart';
+import 'package:morning_weakers/models/models.dart';
 
 class ProfileDetailController extends StateNotifier<ProfileDetailState>
     with LocatorMixin {
@@ -14,7 +15,30 @@ class ProfileDetailController extends StateNotifier<ProfileDetailState>
   Future<void> getProfileDetail() async {
     await Future<void>.delayed(const Duration(seconds: 2));
 
-    final user = dummyUser();
+    const user = User(
+      id: 'dummy',
+      fullName: 'dummy',
+      displayName: 'dummmy',
+      technicalStacks: [
+        TechnicalStack(
+          id: 'dummy',
+          stack: Stack.iOS,
+          languages: [Language.Kotlin, Language.Swift],
+          proficiency: Proficiency.Good,
+          priority: 1,
+        ),
+        TechnicalStack(
+          id: 'dummy',
+          stack: Stack.Android,
+          languages: [Language.Kotlin, Language.Swift],
+          proficiency: Proficiency.Good,
+          priority: 1,
+        ),
+      ],
+      githubAccount: 'dummy',
+      twitterAccount: 'dummy',
+      iconUrl: 'dummy',
+    );
 
     state = state.copyWith(user: user);
   }
