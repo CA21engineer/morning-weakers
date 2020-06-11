437aab206c4ebc07bc27999eb8663e310026591d
Ryuichi Fukami <fukaryu421@gmail.com>
2020-06-11 23:48:00 +0900

    modify: use repository in controller

--- lib/pages/state_management_sample/state_management_sample_controller.dart
+++ lib/pages/state_management_sample/state_management_sample_controller.dart
@@ -1,22 +1,23 @@
 import 'package:morning_weakers/pages/state_management_sample/state_management_sample_state.dart';
 import 'package:state_notifier/state_notifier.dart';
-import 'package:morning_weakers/models/models.dart';
-import 'package:morning_weakers/core/dummy_data.dart';
+import 'package:morning_weakers/models/notifier_state.dart';
+import 'package:morning_weakers/repository/state_management_sample_repository.dart';
 
 class StateManagementSampleController
     extends StateNotifier<StateManagementSampleState> with LocatorMixin {
   StateManagementSampleController() : super(const StateManagementSampleState());
 
-//  @override
-//  void initState() {
-//    getProfileDetail();
-//  }
-//
-//  Future<void> getProfileDetail() async {
-//    await Future<void>.delayed(const Duration(seconds: 2));
-//
-//    final User user = dummyUser();
-//
-//    state = state.copyWith(user: user);
-//  }
+  StateManagementSampleRepository get stateManagementSampleRepository =>
+      read<StateManagementSampleRepository>();
+
+  @override
+  void initState() {
+    getProfileDetail();
+  }
+
+  Future<void> getProfileDetail() async {
+    state = state.copyWith(notifierState: NotifierState.loading);
+    await stateManagementSampleRepository.waitFewSecond();
+    state = state.copyWith(notifierState: NotifierState.loaded);
+  }
 }
