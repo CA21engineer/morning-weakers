0f69202f16e112d5bd99da41a90222d146e9e550
Ryuichi Fukami <fukaryu421@gmail.com>
2020-06-12 01:00:59 +0900

    init: content of AdminGroupEditPage

--- lib/pages/admin_group_edit/admin_group_edit_page.dart
+++ lib/pages/admin_group_edit/admin_group_edit_page.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class AdminGroupEditPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('参加者グループ作成'),
+      ),
+      body: Container(),
+    );
   }
 }
