9462f738036513247ce5d38264d4f9eece771408
Ryuichi Fukami <fukaryu421@gmail.com>
2020-06-11 14:47:50 +0900

    add: profile_detail_controller

---
+++ lib/pages/profile_detail/profile_detail_controller.dart
@@ -0,0 +1,44 @@
+import 'package:morning_weakers/pages/profile_detail/profile_detail_state.dart';
+import 'package:state_notifier/state_notifier.dart';
+import 'package:morning_weakers/models/models.dart';
+
+class ProfileDetailController extends StateNotifier<ProfileDetailState>
+    with LocatorMixin {
+  ProfileDetailController() : super(const ProfileDetailState());
+
+  @override
+  void initState() {
+    getProfileDetail();
+  }
+
+  Future<void> getProfileDetail() async {
+    await Future<void>.delayed(const Duration(seconds: 2));
+
+    const user = User(
+      id: 'dummy',
+      fullName: 'dummy',
+      displayName: 'dummmy',
+      technicalStacks: [
+        TechnicalStack(
+          id: 'dummy',
+          stack: Stack.iOS,
+          languages: [Language.Kotlin, Language.Swift],
+          proficiency: Proficiency.Good,
+          priority: 1,
+        ),
+        TechnicalStack(
+          id: 'dummy',
+          stack: Stack.Android,
+          languages: [Language.Kotlin, Language.Swift],
+          proficiency: Proficiency.Good,
+          priority: 1,
+        ),
+      ],
+      githubAccount: 'dummy',
+      twitterAccount: 'dummy',
+      iconUrl: 'dummy',
+    );
+
+    state = state.copyWith(user: user);
+  }
+}
