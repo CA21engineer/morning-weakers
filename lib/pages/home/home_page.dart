d20fb5ed17828cd8debed6310d6afc0e8ed892b0
zoothezoo <kohekoshien@gmail.com>
2020-06-11 22:32:06 +0900

    add: source link widget logic

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -5,6 +5,7 @@ import 'package:morning_weakers/pages/home/drawer/home_drawer.dart';
 import 'package:morning_weakers/pages/home/home_controller.dart';
 import 'package:morning_weakers/pages/home/home_state.dart';
 import 'package:morning_weakers/pages/home/widget/data_table.dart';
+import 'package:morning_weakers/pages/home/widget/source_link_widget.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -21,7 +22,7 @@ class HomePage extends StatelessWidget {
               padding: const EdgeInsets.all(20),
               children: <Widget>[
                 DataTableView(),
-                _sourceLinks(),
+                SourceLinkWidget(),
                 const Divider(),
                 _allGroupNav(context),
               ],
@@ -30,47 +31,6 @@ class HomePage extends StatelessWidget {
         ),
         drawer: HomeDrawer(),
       ),
-    );
-  }
-
-  final List<List<String>> _data = [
-    ['鈴木', 'Suzu-K', 'twi-suzu', 'git-suzu', 'All'],
-    ['佐藤', 'Sato-U', 'twi-sato', 'git-sato', 'All'],
-    ['田中', 'Tana-K', 'twi-tana', 'git-tana', 'All'],
-    ['高橋', 'Taka-C', 'twi-taka', 'git-taka', 'All'],
-  ];
-
-  //Github/slideなど資料のリンク置き場
-  Widget _sourceLinks() {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 10),
-      child: Column(children: const <Widget>[
-        Text(
-          '資料リンク',
-          style: TextStyle(
-            fontSize: 20,
-          ),
-        ),
-        ListTile(
-            title: Text(
-              'Github',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            subtitle: Text(
-              'https://github.com/CA21engineer/morning-weakers',
-              style: TextStyle(fontStyle: FontStyle.italic),
-            )),
-        ListTile(
-          title: Text(
-            'Slide',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          subtitle: Text(
-            'https://docs.google.com/presentation/morning-weakers',
-            style: TextStyle(fontStyle: FontStyle.italic),
-          ),
-        ),
-      ]),
     );
   }
 
