9ad458ac8c90598f8b963db7109969d7e77fafbc
Ryuichi Fukami <fukaryu421@gmail.com>
2020-06-11 18:46:40 +0900

    modify: change to manage loading status

--- lib/pages/profile_detail/widgets/profile_container.dart
+++ lib/pages/profile_detail/widgets/profile_container.dart
@@ -3,55 +3,61 @@ import 'package:morning_weakers/pages/profile_detail/widgets/profile_list_contai
 import 'package:morning_weakers/pages/profile_detail/profile_detail_state.dart';
 import 'package:provider/provider.dart';
 import 'package:morning_weakers/pages/profile_detail/widgets/technical_stack_container.dart';
+import 'package:morning_weakers/models/models.dart';
 
 class ProfileContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Container(
-                decoration: const BoxDecoration(
-                    shape: BoxShape.circle, color: Colors.blue),
-                child: const CircleAvatar(
-                  backgroundColor: Colors.blue,
-                  radius: 35,
+    if (context.select<ProfileDetailState, User>((state) => state.user) ==
+        null) {
+      return const Center(child: CircularProgressIndicator());
+    } else {
+      return Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Container(
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.blue),
+                  child: const CircleAvatar(
+                    backgroundColor: Colors.blue,
+                    radius: 35,
+                  ),
                 ),
-              ),
-              const SizedBox(width: 20),
-              Text(
-                '${context.select<ProfileDetailState, String>((state) => state.user.fullName)}',
-                style: const TextStyle(fontSize: 30),
-              ),
-            ],
-          ),
-          const SizedBox(height: 20),
-          ProfileListContainer(
-            titleName: 'ハンドルネーム',
-            contentName:
-                '${context.select<ProfileDetailState, String>((state) => state.user.displayName)}',
-          ),
-          const Divider(),
-          ProfileListContainer(
-            titleName: 'Twitter',
-            contentName:
-                '${context.select<ProfileDetailState, String>((state) => state.user.twitterAccount)}',
-          ),
-          const Divider(),
-          ProfileListContainer(
-            titleName: 'Github',
-            contentName:
-                '${context.select<ProfileDetailState, String>((state) => state.user.githubAccount)}',
-          ),
-          const Divider(),
-          TechnicalStackContainer(),
-          const Divider(),
-        ],
-      ),
-    );
+                const SizedBox(width: 20),
+                Text(
+                  '${context.select<ProfileDetailState, String>((state) => state.user.fullName)}',
+                  style: const TextStyle(fontSize: 30),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            ProfileListContainer(
+              titleName: 'ハンドルネーム',
+              contentName:
+                  '${context.select<ProfileDetailState, String>((state) => state.user.displayName)}',
+            ),
+            const Divider(),
+            ProfileListContainer(
+              titleName: 'Twitter',
+              contentName:
+                  '${context.select<ProfileDetailState, String>((state) => state.user.twitterAccount)}',
+            ),
+            const Divider(),
+            ProfileListContainer(
+              titleName: 'Github',
+              contentName:
+                  '${context.select<ProfileDetailState, String>((state) => state.user.githubAccount)}',
+            ),
+            const Divider(),
+            TechnicalStackContainer(),
+            const Divider(),
+          ],
+        ),
+      );
+    }
   }
 }
