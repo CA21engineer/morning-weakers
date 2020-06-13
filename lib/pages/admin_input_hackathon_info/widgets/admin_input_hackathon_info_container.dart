eef638dbf5cd5e8a3f4c5a58dc14af2acc201289
Ryuichi Fukami <fukaryu421@gmail.com>
2020-06-13 22:38:48 +0900

    add: AdminInputHackathonInfoContainer

--- lib/pages/admin_input_hackathon_info/widgets/admin_input_hackathon_info_container.dart
+++ lib/pages/admin_input_hackathon_info/widgets/admin_input_hackathon_info_container.dart
@@ -4,22 +4,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_state_notifier/flutter_state_notifier.dart';
 import 'package:provider/provider.dart';
 import 'package:morning_weakers/repository/state_management_sample_repository.dart';
-//import 'package:morning_weakers/pages/admin_input_hackathon_info/admin_input_hackathon_info_state.dart';
-//import 'package:morning_weakers/pages/admin_input_hackathon_info/admin_input_hackathon_info_controller.dart';
+//import 'package:morning_weakers/pages/admin_input_hackathon_info_container/admin_input_hackathon_info_state.dart';
+//import 'package:morning_weakers/pages/admin_input_hackathon_info_container/admin_input_hackathon_info_controller.dart';
 import 'package:morning_weakers/pages/admin_input_hackathon_info/widgets/admin_input_hackathon_info_container.dart';
 import 'package:morning_weakers/models/models.dart';
 
-class AdminInputHackathonInfoPage extends StatelessWidget {
+class AdminInputHackathonInfoContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('ハッカソン概要入力'),
-      ),
-      body: Provider(
-        create: (_) => StateManagementSampleRepository(),
-        child: AdminInputHackathonInfoContainer(),
-      ),
-    );
+    return Container();
   }
 }
