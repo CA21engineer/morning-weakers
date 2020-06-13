f3e48b25e09d4a94d8a8c16507c5eb2599f9df5a
zoothezoo <kohekoshien@gmail.com>
2020-06-13 22:14:33 +0900

    fix: PR

--- lib/pages/group_result/group_result_card_widget.dart
+++ lib/pages/group_result/group_result_card_widget.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:morning_weakers/pages/group_result/group_result_state.dart';
-import 'package:provider/provider.dart';
-import 'package:morning_weakers/models/models.dart';
 
 //TODO: cardの中身でwidget切り分けた方がいいかな？いらなければPR時に消します.
 class GroupResultCardWidget extends StatelessWidget {
