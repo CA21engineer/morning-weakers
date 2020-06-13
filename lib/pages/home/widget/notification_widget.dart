5a391156239dff74a2fd5c4b29b747a1da8b5596
zoothezoo <kohekoshien@gmail.com>
2020-06-13 21:46:06 +0900

    add: logic

--- lib/pages/home/widget/notification_widget.dart
+++ lib/pages/home/widget/notification_widget.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:morning_weakers/models/models.dart' as model;
+import 'package:morning_weakers/pages/home/home_state.dart';
+import 'package:provider/provider.dart';
 
 class NotificationWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final List<model.Notification> notification = context.select<HomeState, List<model.Notification>>(
+      (state) => state.hackathon.notifications,
+    );
+    // ignore: cascade_invocations
+    notification.sort((a, b) => a.createdAt.compareTo(b.createdAt));
+
     return Container(
         padding: const EdgeInsets.all(8),
         decoration: BoxDecoration(
@@ -11,9 +20,9 @@ class NotificationWidget extends StatelessWidget {
           borderRadius: BorderRadius.circular(10),
           border: Border.all(color: Colors.cyan[600]),
         ),
-        child: const ListTile(
-          title: Text('[緊急] 発表日変更'),
-          subtitle: Text('ごめんなさい！突然ですが、やっぱり発表日を明日に変更します！'),
+        child: ListTile(
+          title: Text(notification[0].title),
+          subtitle: Text(notification[0].description),
           trailing: Icon(Icons.arrow_forward_ios),
         ));
   }
