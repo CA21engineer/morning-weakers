90cd3618572ed844af11ab9b615b5c87df28bea9
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2020-06-14 03:04:19 +0900

    Add: 【参加者】参加者アンケート➡️ グループ確認

--- lib/pages/input_participant_info/widgets/submit_btn.dart
+++ lib/pages/input_participant_info/widgets/submit_btn.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:morning_weakers/pages/group_result/group_result_page.dart';
 
 class SubmitBtn extends StatelessWidget {
   @override
@@ -10,6 +11,7 @@ class SubmitBtn extends StatelessWidget {
       color: Theme.of(context).accentColor,
       onPressed: () async {
 //        await context.read<InputParticipantInfoController>().handlePost();
+        await Navigator.push<void>(context, MaterialPageRoute(builder: (_) => GroupResultPage()));
       });
   }
 }
