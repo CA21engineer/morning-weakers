5befe7d64f947bdd89d96b0a311b0fa3cd0529ee
Ryuichi Fukami <fukaryu421@gmail.com>
2020-06-13 17:11:54 +0900

    add: AdminGroupEditController

---
+++ lib/pages/admin_group_edit/admin_group_edit_controller.dart
@@ -0,0 +1,22 @@
+import 'package:morning_weakers/pages/admin_group_edit/admin_group_edit_state.dart';
+import 'package:state_notifier/state_notifier.dart';
+import 'package:morning_weakers/models/notifier_state.dart';
+import 'package:morning_weakers/models/models.dart';
+
+class AdminGroupEditController extends StateNotifier<AdminGroupEditState> with LocatorMixin {
+  AdminGroupEditController() : super(const AdminGroupEditState());
+
+  // TODO: Repositoy
+  //  StateManagementSampleRepository get stateManagementSampleRepository => read<StateManagementSampleRepository>();
+
+  void setEditedGroups(List<Participant> participants) {
+    state = state.copyWith(groups: participants.setEditedGroups());
+  }
+
+  Future<void> postGroups(List<Group> groups) async {
+    state = state.copyWith(notifierState: NotifierState.loading);
+    // TODO: Repositoryのmethod
+//    await stateManagementSampleRepository.waitFewSecond();
+    state = state.copyWith(notifierState: NotifierState.loaded);
+  }
+}
